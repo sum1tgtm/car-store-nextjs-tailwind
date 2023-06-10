083bbfafa30ae9a261c14bba48b324a3454c815b
Sumit Gautam <thisepic123@gmail.com>
2023-06-10 19:01:06 +0545

    search filter

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -3,14 +3,25 @@ import { SearchManufacturerProps } from "@/types";
 import { Combobox, Transition } from "@headlessui/react";
 import Image from "next/image";
 import { useState, Fragment } from "react";
+import { manufacturers } from "@/constants";
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
   const [query, setQuery] = useState("");
+
+  const filteredManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px] ">
             <Image
@@ -29,8 +40,57 @@ const SearchManufacturer = ({
               setQuery(e.target.value);
             }}
           />
-          <Transition>
-             
+          <Transition
+            as={Fragment}
+            leave="Transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => {
+              setQuery("");
+            }}
+          >
+            <Combobox.Options>
+              {/* for creating a new option | but we dont have to create a new option in this project */}
+              {/* {filteredManufacturers.length === 0 && query !== "" ? ( 
+                <Combobox.Option
+                  value={query}
+                  className="search-manufacturer__option"
+                >
+                  Create "{query}"
+                </Combobox.Option>
+              ) : ( */}
+              {filteredManufacturers.map((item) => (
+                <Combobox.Option
+                  key={item}
+                  className={({ active }) =>
+                    `relative search-manufacturer__option ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    }`
+                  }
+                  value={item}
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {item}
+                      </span>
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? "text-white" : "text-teal-600"
+                          }`}
+                        ></span>
+                      ) : null}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))}
+              )
+            </Combobox.Options>
           </Transition>
         </div>
       </Combobox>
